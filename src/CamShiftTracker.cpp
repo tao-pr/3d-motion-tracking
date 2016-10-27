c2b943e5f7f2d0aeaf3f0a71eb2d0f7196f9eab9
starcolon <tao@starcolon.com>
2016-10-27 21:56:55 +0700

    Tidy up a bit

--- src/CamShiftTracker.cpp
+++ src/CamShiftTracker.cpp
@@ -23,20 +23,20 @@ void CamShiftTracker::trackMotion(Mat &im)
 
   // Blur the image for noise reduction
   Mat bhsv;
-  GaussianBlur( hsv, bhsv, Size(7,7), 8.0, 8.0, BORDER_REFLECT_101);
+  GaussianBlur( hsv, bhsv, Size(9,9), 8.0, 8.0, BORDER_REFLECT_101);
 
   // Compute image histogram
   cout << "...Computing histogram" << endl;
   Mat hist, backproj;
   Mat mask              = Mat(); // No masking
-  int histBins[]        = {32, 32}; // bin sizes
+  int histBins[]        = {64, 64}; // bin sizes
   float range_hue[]     = {0, 180};
   float range_sat[]     = {0, 256};
   const float* ranges[] = {range_hue, range_sat};
   int nChannels[]       = {0, 2}; // Index of channels to take
 
   calcHist( &hsv, 1, nChannels, mask, hist, 2, histBins, ranges, true, false );
-  normalize(hist, hist, 0, 32, NORM_MINMAX);
+  normalize(hist, hist, 0, 255, NORM_MINMAX, -1, Mat());
   
   cout << "...Computing back projection" << endl;
   calcBackProject(&hsv, 1, nChannels, hist, backproj, ranges);
@@ -46,7 +46,7 @@ void CamShiftTracker::trackMotion(Mat &im)
   RotatedRect tracked  = CamShift(backproj, entireRect, crit);
 
   // Draw the tracked area
-  auto canvas = bhsv.clone();
+  auto canvas = im.clone();
   ellipse( canvas, tracked, Scalar(0,100,255), 3, LINE_AA );
 
   // Draw the tracked movement direction
@@ -55,7 +55,7 @@ void CamShiftTracker::trackMotion(Mat &im)
     tracked.center, 
     Point(
       tracked.center.x + tracked.size.width*cos(tracked.angle)/5,
-      tracked.center.y - tracked.size.width*sin(tracked.angle)/5),
+      tracked.center.y - tracked.size.height*sin(tracked.angle)/5),
     Scalar(0,100,255),
     2, 8, 0, 0.08
     );
