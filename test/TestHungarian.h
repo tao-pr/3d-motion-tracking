42d4ebce927db136e01f239ca420eeb0bf34d927
starcolon <tao@starcolon.com>
2016-12-11 03:09:25 +0700

    (WIP) [Test] Print out zero lines

--- test/TestHungarian.h
+++ test/TestHungarian.h
@@ -14,11 +14,20 @@ function<bool()> caseCoverZeros = []()
   float vecM[9] = {0, 1, 5, 4, 2, 1, 3, 3, 0};
   Mat m         = Mat(3, 3, CV_32F, vecM);
 
+  cout << m << endl << endl;
+
   tuple<set<int>, set<int>> zeroes = Hungarian::coverZeros(m);
   set<int> lineRows = get<0>(zeroes);
   set<int> lineCols = get<1>(zeroes);
 
   // TAOTODO:
+  cout << "rows: " << endl;
+  for (auto j : lineRows) 
+    cout << j << endl;
+
+  cout << "cols: " << endl;
+  for (auto i : lineCols)
+    cout << i << endl;
 
   return true;
 };
