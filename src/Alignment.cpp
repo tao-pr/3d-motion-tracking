f19bba79bb7d89b5d5fd01019ec627b5ae46903e
starcolon <tao@starcolon.com>
2017-06-14 01:46:35 +0700

    track : (WIP) record distance score mapping

--- src/Alignment.cpp
+++ src/Alignment.cpp
@@ -10,6 +10,7 @@ Alignment::Alignment(function<double (Point2f, Point2f)> measureDistance, functi
 void Alignment::align(vector<Point2f> basepoints, vector<Point2f> newpoints, const Mat& baseFeatures, const Mat& newFeatures) const
 {
   int i = 0;
+  Mat matchScore = Mat(basepoints.size(), newpoints.size(), CV_32FC1, Scalar(1e32));
   for (auto bp : basepoints)
   {
     // List of Tuples of <distance, index of candidate>
@@ -27,11 +28,16 @@ void Alignment::align(vector<Point2f> basepoints, vector<Point2f> newpoints, con
 
     // Take only top [N] closest new points
     const int N = 3;
-    queue<distanceToIndex> qClosests;
-    while (qClosests.size() < N && !qClosests.empty())
+    queue<distanceToIndex> q;
+    while (!candidates.empty() && q.size()<N)
     {
-      qClosests.push(candidates.top());
+      auto c = candidates.top();
+      q.push(c);
       // TAOTODO: Measure similarity between [baseFeature[i]] vs [newFeature[j]]
+
+      // Record the score (invert distance)
+      int j = get<0>(c);
+      matchScore.at<float>(i,j) = 1/get<1>(c);
     }
 
     // TAOTODO:
