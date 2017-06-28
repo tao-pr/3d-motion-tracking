d477b332d9d8f00f57aad240d3cb4eb3e6395745
starcolon <tao@starcolon.com>
2017-06-29 00:43:48 +0700

    [grid] draft

--- include/Grid.h
+++ include/Grid.h
@@ -14,18 +14,28 @@ typedef function<double (double)> GravityFunction ;
 class Grid
 {
 protected:
-  Mat velocity;
+  Mat velocityX, velocityY;
   GravityFunction gravity; // Function which maps [distance] => [gravity magnitude]
   double gravityThreshold; // Minimum gravity magnitude to draw effect
 
 public:
   Grid(Size gridSize, GravityFunction gravity, double gravityThreshold = 0.0)
   {
-    this->velocity = Mat::zeros(gridSize.height, gridSize.width, 3);
-    this->GravityFunction = gravity;
+    this->velocityX = Mat::zeros(gridSize.height, gridSize.width, CV_64FC1);
+    this->velocityY = Mat::zeros(gridSize.height, gridSize.width, CV_64FC1);
+    this->gravity   = gravity;
     this->gravityThreshold = gravityThreshold;
   }
-  ~Grid();
+  
+  virtual ~Grid()
+  {
+  }
+
+  inline void setAnchor(int x, int y, const Point2f& velocity)
+  {
+    this->velocityX.at<double>(y, x) = velocity.x;
+    this->velocityY.at<double>(y, x) = velocity.y;
+  }
 };
 
 
